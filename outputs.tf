abd6f43d163404b8a3649de87bfe174d3f98bc08
Fabrice Dall'ara <fabrice.dallara@hotmail.com>
2023-01-16 15:15:23 +0100

    Adding support for User Managed identities

--- outputs.tf
+++ outputs.tf
@@ -17,3 +17,13 @@ output "acr_fqdn" {
   description = "The Container Registry FQDN."
   value       = azurerm_container_registry.registry.login_server
 }
+
+output "id_acr_pull" {
+  description = "Id to the scope  the user-managed identity with ACrPull Role"
+  value = azurerm_user_assigned_identity.acr_pull.id
+}
+
+output "id_acr_push" {
+  description = "Id to the scope  the user-managed identity with ACrPush Role"
+  value = azurerm_user_assigned_identity.acr_push.id
+}
