63925b9e425e2ea46caa3c8b6e628b0594ef0ed1
Fabrice Dall'ara <fabrice.dallara@hotmail.com>
2023-02-12 12:36:32 +0100

    Fixing bug

--- locals-naming.tf
+++ locals-naming.tf
@@ -8,7 +8,7 @@ locals {
   location_short         = var.location_short
   separator              = ""  # ACR do not support dashes in the resource name
   generated_random_value = random_string.random.result
-  name_components_list   = compact([local.service_slug, local.landing_zone_slug, location_short, local.application_id, local.workload_info, local.generated_random_value])
+  name_components_list   = compact([local.service_slug, local.landing_zone_slug, local.location_short, local.application_id, local.workload_info, local.generated_random_value])
   standard_name          = join(local.separator, local.name_components_list)
   name                   = coalesce(lower(var.custom_name), lower(local.standard_name))
 }
